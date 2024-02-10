e0ff7dc6530631f5b4a4e242d99b3718596d7d75
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-02-10 12:36:32 +0100

    added synced defaults to merchant script

--- merchant.lua
+++ merchant.lua
@@ -7,3 +7,27 @@
 
 local configFile = "OpenBankingConfig/merchantConfig.txt" -- Config path here
 local defaultConfig = {} -- Default config here
+
+------------------------------------------THIS SECTION SHOULD BE SYNCED BETWEEN ALL OPENBANKING SOFTWARE------------------------------------------
+-- load config
+if not fs.exists(configFile) then
+    local fh = fs.open(configFile, "w")
+    fh.write(textutils.serialize(defaultConfig))
+end
+local configHandle = fs.open(configFile, "r")
+local configData = textutils.unserialize(configHandle.readAll())
+configHandle.close()
+
+-- standard data
+local stdDATELOCALE = "utc"
+local stdEPOCH = function () return os.epoch(stdDATELOCALE) end
+
+-- build OpenBanking data
+local OBdataPrefix = "OpenBanking:"
+local OBtransactionIDrequestMessage = OBdataPrefix.."requestTransactionID"
+local OBtransactionIDrequestProtocol = OBdataPrefix.."transactionIDRequest"
+local OBtransactionProtocol = OBdataPrefix.."requestTransaction"
+local OBhostProtocol = OBdataPrefix.."BankComms"
+local OBhostnamePrefix = OBdataPrefix.."BankServer:"
+local OBhostname = function (bankname) return OBhostnamePrefix..bankname end
+--------------------------------------------------------------------------------------------------------------------------------------------------
