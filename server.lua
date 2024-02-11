2015691aa6cd68e11633e13ee07aa14db0e6d617
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-02-11 15:08:19 +0100

    added transaction id system

--- server.lua
+++ server.lua
@@ -33,10 +33,17 @@ local OBhostProtocol = OBdataPrefix.."BankComms"
 local OBhostnamePrefix = OBdataPrefix.."BankServer:"
 local OBhostname = function (bankname) return OBhostnamePrefix..bankname end
 --------------------------------------------------------------------------------------------------------------------------------------------------
+-- Server-exclusive data
+--transaction IDs look like this: OpenBanking:transaction:SomeBank@18204120240210-4853
+local OBtransactionID = function () return OBdataPrefix.."transaction:"..configData.bankName.."@"..os.date("%H%M%S%Y%m%d").."-"..math.random(9999) end
 
 -------------------------------------------------------------------- UTILITY ---------------------------------------------------------------------
-function table.contains(table, element)
-    for _, value in pairs(table) do if value == element then return true end end return false -- one liner functions go brrrrr
+function table.contains(t, element, filter)
+    --this is gonna be a damn nightmare to maintain but i want it to be single-line because i am acoustic
+    for _, value in ipairs(t) do if (filter and filter(value)) or (value == element) then return true end end return false
+end
+function table.indexOf(t, element)
+    for i=1,#t do if t[i]==element then return i end end return -1
 end
 function table.keys(t)
     local keys={} for key,_ in pairs(t) do table.insert(keys, key) end return keys
@@ -44,6 +51,22 @@ end
 --------------------------------------------------------------------------------------------------------------------------------------------------
 
 local accounts = {} --STORES ALL ACCOUNT DATA
+
+local transactionIDs = {} --Stores generated transaction ids and info
+
+
+local function generateTransactionID()
+    local genid = OBtransactionID()
+    if table.contains(transactionIDs, nil,
+        function(v) return v.content == genid end ) then --if an identical id is already present
+        return generateTransactionID() --try again until the stack overflows (🤓☝️"but its bad practice to–" yes i know shut up)
+    end
+    transactionIDs[genid] = { --add to transaction IDs list
+        content = genid,
+        expiry = stdEPOCH() + 60000 --set expiry to one minute from now (60000 milliseconds = 60 seconds)
+    }
+    return transactionIDs[genid]
+end
 
 local function runTransaction(from, to, amount)
     local accountfrom = accounts[from]
@@ -59,8 +82,7 @@ local function runTransaction(from, to, amount)
     if accountfrom.balance < amount then --the account has sufficient funds
         return false, "Insufficient balance" end
     if accountfrom.transactionPolicies.maxSingleTransactionAmount < amount then
-        return false, "Transaction amount exceeds single transaction limit"
-    end
+        return false, "Transaction amount exceeds single transaction limit" end
     if table.contains(accountfrom.transactionPolicies.blacklist, accountto) then --the recipient is not blacklisted
         return false, "Recipient blacklisted" end
 
@@ -72,9 +94,8 @@ end
 local function OBhostProtocolMessageHandler(senderID, request, protocol)
 end
 
-local transactionIDoffset = 0 --ensure that every transaction id is unique
 local function OBtransactionIDrequestHandler(senderID, request, protocol)
-
+    return generateTransactionID()
 end
 
 local function OBtransactionRequestHandler(senderID, request, protocol)
@@ -83,7 +104,7 @@ local function OBtransactionRequestHandler(senderID, request, protocol)
         from = request.from,
         to = request.to,
         origin = senderID,
-        amount = request.amount,
+        amount = 0, --transfer nothing by default
         completed = false, --deny transaction by default
         details = "",
         date = os.date("%d-%m-%Y"),
@@ -93,15 +114,22 @@ local function OBtransactionRequestHandler(senderID, request, protocol)
     if senderID ~= request.client then
         response.details = "Client ID is different than declared, possible third party intermission"
         return response end
-    if not table.contains(accounts[request.from].logins, senderID) then
-        response.details = "Client is not logged into funding account"
+    if not transactionIDs[request.id] then
+        response.details = "Invalid transaction ID"
         return response end
-    
+    if stdEPOCH() > transactionIDs[request.id].expiry then
+        response.details = "Transaction ID has expired"
+        return response end
+    if not table.contains(accounts[request.from].sessions, senderID) then
+        response.details = "Client does not have an open session on funding account"
+        return response end
 
     -- run the actual transaction
     local success, info, description = runTransaction(request.from, request.to, request.amount)
-    request.completed = success
-    request.details = info
+    -- remove the transaction ID (make it no longer valid + free up memory)
+    table.remove(transactionIDs, table.indexOf(transactionIDs, request.id))
+    response.completed = success
+    response.details = info
     return response
 end
 
